b6636e48dee3e21f1ac1f516e8d3b17ffca273fc
William Swinny <williamwswinny@gmail.com>
2016-03-03 16:09:45 -0600

    Initial commit

---
+++ libs/SSBA-3.0/build/CMakeFiles/V3D.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/swinny/Desktop/AutonomousFlight-master/sfm/Dependencies/SSBA-3.0/Geometry/v3d_metricbundle.cpp" "/Users/swinny/Desktop/AutonomousFlight-master/sfm/Dependencies/SSBA-3.0/build/CMakeFiles/V3D.dir/Geometry/v3d_metricbundle.cpp.o"
+  "/Users/swinny/Desktop/AutonomousFlight-master/sfm/Dependencies/SSBA-3.0/Geometry/v3d_stereobundle.cpp" "/Users/swinny/Desktop/AutonomousFlight-master/sfm/Dependencies/SSBA-3.0/build/CMakeFiles/V3D.dir/Geometry/v3d_stereobundle.cpp.o"
+  "/Users/swinny/Desktop/AutonomousFlight-master/sfm/Dependencies/SSBA-3.0/Math/v3d_nonlinlsq.cpp" "/Users/swinny/Desktop/AutonomousFlight-master/sfm/Dependencies/SSBA-3.0/build/CMakeFiles/V3D.dir/Math/v3d_nonlinlsq.cpp.o"
+  "/Users/swinny/Desktop/AutonomousFlight-master/sfm/Dependencies/SSBA-3.0/Math/v3d_optimization.cpp" "/Users/swinny/Desktop/AutonomousFlight-master/sfm/Dependencies/SSBA-3.0/build/CMakeFiles/V3D.dir/Math/v3d_optimization.cpp.o"
+  "/Users/swinny/Desktop/AutonomousFlight-master/sfm/Dependencies/SSBA-3.0/Math/v3d_optimization_lm.cpp" "/Users/swinny/Desktop/AutonomousFlight-master/sfm/Dependencies/SSBA-3.0/build/CMakeFiles/V3D.dir/Math/v3d_optimization_lm.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "V3DLIB_ENABLE_SUITESPARSE"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../SuiteSparse_config"
+  "../."
+  "../COLAMD/Include"
+  "/usr/include/suitesparse"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
